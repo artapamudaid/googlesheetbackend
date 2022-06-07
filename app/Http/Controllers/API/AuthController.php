2d8de08bc1124f5d04303b57eea6dab55861a281
artapamudaid <pamuda.id@gmail.com>
2022-06-07 20:59:13 +0700

    create login api

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -26,6 +28,27 @@ class AuthController extends Controller
         return response()
             ->json([
                 'data' => $user,
+                'token' => $token,
+                'token_type' => 'Bearer'
+            ]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $request->validated();
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()
+                ->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json([
+                'message' => 'Hi ' . $user->name . ', have a nice day :)',
                 'token' => $token,
                 'token_type' => 'Bearer'
             ]);
